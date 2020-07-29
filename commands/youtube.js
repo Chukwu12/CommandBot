1e1fab747a64618ef0f402a02d2e82eac6988e0d
okey <39450490+Chukwu12@users.noreply.github.com>
2020-07-29 11:19:31 -0400

    Add files via upload

---
+++ commands/youtube.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "youtube",
+  description: "sens the youtube link!",
+  execute(message, args) {
+    message.channel.send(
+      "https://www.youtube.com/watch?v=jTA0R2_cM-0&t=2s!/code1yon"
+    );
+  },
+};
